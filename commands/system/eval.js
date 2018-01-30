a10b865b5dd2632639e504d8f76cdb1c0794dbaa
Yukine <blazylp@gmail.com>
2018-01-30 17:40:41 +0100

    update eval to await bluebird/snekfetch Promises (#68)
    * improved eval to await snekfetch/bluebird promises
    
    * updated eval to stay consistent

--- commands/system/eval.js
+++ commands/system/eval.js
@@ -24,7 +24,7 @@ class Eval extends Command {
     const filter = new RegExp(`${token}|${rev}`, 'g');
     try {
       let output = eval(code);
-      if (output instanceof Promise) output = await output;
+      if (output instanceof Promise || (Boolean(output) && typeof output.then === 'function' && typeof output.catch === 'function')) output = await output;
       output = inspect(output, { depth: 0, maxArrayLength: null });
       output = output.replace(filter, '[TOKEN]');
       output = clean(output);
